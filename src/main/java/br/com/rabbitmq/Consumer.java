3622e0fb00c972993222ddaba8b71496d464eaf8
jonatasrodrigues <jsrodrigues@hotmail.com>
2019-09-07 20:57:13 -0300

    first commit

---
+++ src/main/java/br/com/rabbitmq/Consumer.java
@@ -0,0 +1,25 @@
+package br.com.rabbitmq;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Consumer {
+
+	public static void main(String[] args) throws IOException, TimeoutException {
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		
+		DeliverCallback callback = (consumerTag, delivery) -> {
+			String message = new String (delivery.getBody());
+			System.out.println(message);
+		};
+		
+		channel.basicConsume("Queue-1",true, callback, consumerTag -> {});
+	}
+}
